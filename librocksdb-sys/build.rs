a29b652a54f9109aa5643c0c5cf0949eaf4f5768
popcnt <142196625+popcnt1@users.noreply.github.com>
2024-11-25 14:37:26 +0800

    fix(build): add ROCKSDB_SCHED_GETCPU_PRESENT for Linux build config (#950)
    * fix(build): add ROCKSDB_SCHED_GETCPU_PRESENT for Linux build config
    
    This change adds a definition for ROCKSDB_SCHED_GETCPU_PRESENT in the Linux-specific section of the build script. This ensures that the RocksDB library is aware of the presence of the sched_getcpu function on Linux systems, potentially optimising CPU scheduling.

--- librocksdb-sys/build.rs
+++ librocksdb-sys/build.rs
@@ -168,6 +168,11 @@ fn build_rocksdb() {
         config.define("OS_LINUX", None);
         config.define("ROCKSDB_PLATFORM_POSIX", None);
         config.define("ROCKSDB_LIB_IO_POSIX", None);
+        config.define("ROCKSDB_SCHED_GETCPU_PRESENT", None);
+    } else if target.contains("dragonfly") {
+        config.define("OS_DRAGONFLYBSD", None);
+        config.define("ROCKSDB_PLATFORM_POSIX", None);
+        config.define("ROCKSDB_LIB_IO_POSIX", None);
     } else if target.contains("freebsd") {
         config.define("OS_FREEBSD", None);
         config.define("ROCKSDB_PLATFORM_POSIX", None);
